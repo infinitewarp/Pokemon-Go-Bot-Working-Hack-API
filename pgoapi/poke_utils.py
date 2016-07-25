83f056534bd6bb5dbf44dff9a6af9d640a5b43b1
darkline81 <darkline81@gmail.com>
2016-07-24 23:21:38 -0700

    Add item info logging and sorted Pokemon names.

--- pgoapi/poke_utils.py
+++ pgoapi/poke_utils.py
@@ -4,6 +4,12 @@ def pokemonIVPercentage(pokemon):
     return ((pokemon.get('individual_attack', 0) + pokemon.get('individual_stamina', 0) + pokemon.get(
         'individual_defense', 0) + 0.0) / 45.0) * 100.0
 
+def get_pokemon_num(res):
+    inventory_delta = res['responses']['GET_INVENTORY'].get('inventory_delta', {})
+    inventory_items = inventory_delta.get('inventory_items', [])
+    inventory_items_dict_list = map(lambda x: x.get('inventory_item_data', {}), inventory_items)
+    inventory_items_pokemon_list = filter(lambda x: 'pokemon_data' in x and 'is_egg' not in x['pokemon_data'], inventory_items_dict_list)
+    return len(inventory_items_pokemon_list)
 
 def get_inventory_data(res, poke_names):
     inventory_delta = res['responses']['GET_INVENTORY'].get('inventory_delta', {})
@@ -11,8 +17,9 @@ def get_inventory_data(res, poke_names):
     inventory_items_dict_list = map(lambda x: x.get('inventory_item_data', {}), inventory_items)
     inventory_items_pokemon_list = filter(lambda x: 'pokemon_data' in x and 'is_egg' not in x['pokemon_data'],
                                           inventory_items_dict_list)
+    inventory_items_pokemon_list_sorted = sorted(inventory_items_pokemon_list, key=lambda x: poke_names[str(x['pokemon_data']['pokemon_id'])].encode('ascii', 'ignore'))
 
     return (os.linesep.join(map(lambda x: "{0}, CP {1}, IV {2:.2f}".format(
         poke_names[str(x['pokemon_data']['pokemon_id'])].encode('ascii', 'ignore'),
         x['pokemon_data']['cp'],
-        pokemonIVPercentage(x['pokemon_data'])), inventory_items_pokemon_list)))
+        pokemonIVPercentage(x['pokemon_data'])), inventory_items_pokemon_list_sorted)))
