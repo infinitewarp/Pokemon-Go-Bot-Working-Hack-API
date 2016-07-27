0742893e4521c52c4c80aa997650067d872a2d01
BoBeR182 <BoBeR182@users.noreply.github.com>
2016-07-27 02:02:06 +0000

    More item tweaks

--- pgoapi/pgoapi.py
+++ pgoapi/pgoapi.py
@@ -27,7 +27,7 @@ from collections import defaultdict
 import os.path
 
 logger = logging.getLogger(__name__)
-BAD_ITEM_IDS = [1,2,3,101,102,103,701,702,703] #Pokeball, Greatball, Ultraball, Potion, Super Potion, Hyper Potion, RazzBerry, BlukBerry, Revive
+BAD_ITEM_IDS = [1,2,3,101,102,103,701,702,703] #Pokeball, Greatball, Ultraball, Potion, Super Potion, Hyper Potion,, max potions RazzBerry, BlukBerry, Revive
 
 # Minimum amount of the bad items that you should have ... Modify based on your needs ... like if you need to battle a gym?
 MIN_BAD_ITEM_COUNTS = {Inventory.ITEM_POKE_BALL: 50,
@@ -36,17 +36,18 @@ MIN_BAD_ITEM_COUNTS = {Inventory.ITEM_POKE_BALL: 50,
                        Inventory.ITEM_POTION: 5,
                        Inventory.ITEM_SUPER_POTION: 5,
                        Inventory.ITEM_HYPER_POTION: 50,
+                       Inventory.ITEM_MAX_POTION: 50,
                        Inventory.ITEM_RAZZ_BERRY: 25,
                        Inventory.ITEM_BLUK_BERRY: 10,
                        Inventory.ITEM_NANAB_BERRY: 10,
-                       Inventory.ITEM_REVIVE: 10}
+                       Inventory.ITEM_REVIVE: 15}
                        
 # Candy needed to evolve pokemon
-CANDY_NEEDED_TO_EVOLVE = {10: 11,#Caterpie
-                          16: 11,#pidgey
-                          13: 11,#Weedle
-                          19: 24,#Rattata
-                          41: 49,#Zubat
+CANDY_NEEDED_TO_EVOLVE = {10: 11, #Caterpie
+                          16: 11, #pidgey
+                          13: 11, #Weedle
+                          19: 24, #Rattata
+                          41: 49, #Zubat
                           }
 MIN_SIMILAR_POKEMON = 1
 INVENTORY_DICT = {Inventory.ITEM_UNKNOWN: "UNKNOWN",
@@ -266,8 +267,8 @@ class PGoApi:
         neighbors = getNeighbors(self._posf)
         return self.get_map_objects(latitude=position[0], longitude=position[1], since_timestamp_ms=[0]*len(neighbors), cell_id=neighbors).call()
 
-    def attempt_catch(self,encounter_id,spawn_point_guid): #Problem here... add 4 if you have master ball
-        for i in range(1,4): # Range 1...4 iff you have master ball `range(1,4)`
+    def attempt_catch(self,encounter_id,spawn_point_guid): #Problem here... add 5 if you have master ball
+        for i in range(1,4): # Range 1...5 iff you have master ball `range(1,5)`
             r = self.catch_pokemon(
                 normalized_reticle_size= 1.950,
                 pokeball = i,
